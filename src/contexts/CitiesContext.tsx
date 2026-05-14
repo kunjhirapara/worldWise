7cf2bfcc17ab4bf8bb5aaf40401d156d9e6752c2
Kunj Hirapara <kunjhirapara2@gmail.com>
2026-05-14 23:39:18 +0530

    fix(path): api routing error solved

--- src/contexts/CitiesContext.tsx
+++ src/contexts/CitiesContext.tsx
@@ -1,7 +1,7 @@
 import {createContext, useContext, useEffect, useReducer} from "react";
 import type {CityState, CityContextType, State, CityAction} from "../type";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 const initialState: State = {
   cities: [],
